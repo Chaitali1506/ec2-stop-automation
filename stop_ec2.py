88555ac16f30dfa703a4390fc906cb38ef29e2f7
Chaitali1506 <chaitalikale1512003@email.com>
2026-08-24 15:45:56 +0530

    add region in script

--- stop_ec2.py
+++ stop_ec2.py
@@ -1,7 +1,7 @@
 import boto3
 
 # Create EC2 client
-ec2 = boto3.client("ec2")
+ec2 = boto3.client("ec2", region_name="ap-south-1")
 
 # Get all running instances
 response = ec2.describe_instances(
